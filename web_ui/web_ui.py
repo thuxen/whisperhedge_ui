81c35a56f49ebdaad2b5d0ae13bf7c39d81ad205
thuxen <thuxen@gmail.com>
2026-02-10 09:25:13 -0500

    Add Google Analytics and Mixpanel tracking to web application - inject analytics scripts into head of every page with GA4 property G-V3S6FLHXV8 and Mixpanel project with autocapture and 100% session recording enabled

--- web_ui/web_ui.py
+++ web_ui/web_ui.py
@@ -18,7 +18,30 @@ from .pages.settings import settings
 from .components import DashboardState
 
 
-app = rx.App()
+# Analytics scripts that will be injected into the head of every page
+analytics_scripts = [
+    # Google Analytics
+    rx.script(src="https://www.googletagmanager.com/gtag/js?id=G-V3S6FLHXV8", custom_attrs={"async": True}),
+    rx.script("""
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-V3S6FLHXV8');
+    """),
+    # Mixpanel
+    rx.script("""
+        (function(e,c){if(!c.__SV){var l,h;window.mixpanel=c;c._i=[];c.init=function(q,r,f){function t(d,a){var g=a.split(".");2==g.length&&(d=d[g[0]],a=g[1]);d[a]=function(){d.push([a].concat(Array.prototype.slice.call(arguments,0)))}}var b=c;"undefined"!==typeof f?b=c[f]=[]:f="mixpanel";b.people=b.people||[];b.toString=function(d){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);d||(a+=" (stub)");return a};b.people.toString=function(){return b.toString(1)+".people (stub)"};l="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders start_session_recording stop_session_recording people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");
+        for(h=0;h<l.length;h++)t(b,l[h]);var n="set set_once union unset remove delete".split(" ");b.get_group=function(){function d(p){a[p]=function(){b.push([g,[p].concat(Array.prototype.slice.call(arguments,0))])}}for(var a={},g=["get_group"].concat(Array.prototype.slice.call(arguments,0)),m=0;m<n.length;m++)d(n[m]);return a};c._i.push([q,r,f])};c.__SV=1.2;var k=e.createElement("script");k.type="text/javascript";k.async=!0;k.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===
+        e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";e=e.getElementsByTagName("script")[0];e.parentNode.insertBefore(k,e)}})(document,window.mixpanel||[])
+        
+        mixpanel.init('2d6698da226871e8b48d1c63a9cc242e', {
+            autocapture: true,
+            record_sessions_percent: 100,
+        })
+    """),
+]
+
+app = rx.App(head_components=analytics_scripts)
 app.add_page(landing_page, route="/", image="/favicon.ico")
 app.add_page(login_page, route="/login", image="/favicon.ico")
 app.add_page(signup_page, route="/signup", image="/favicon.ico")
