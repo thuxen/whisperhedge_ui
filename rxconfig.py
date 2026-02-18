a71be388a5b695ce79c314e9ae9add9c7c2b0d12
thuxen <thuxen@gmail.com>
2026-02-18 04:14:46 -0500

    Add page title to Reflex config for browser tab display

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="web_ui",
+    title="WhisperHedge - Automated Liquidity Pool Hedging",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
